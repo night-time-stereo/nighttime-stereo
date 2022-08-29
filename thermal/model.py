836ded6ab6a1adfc49e0b786ed2df0be850bc605
Vanshaj Chowdhary <vanshaj.chowdhary@gmail.com>
2022-08-29 17:44:19 -0400

    Setup basic skeleton

---
+++ thermal/model.py
@@ -0,0 +1,8 @@
+import torch.nn as nn
+
+class ThermalDepthModel:
+    def __init__(self) -> None:
+        pass
+
+    def forward(self, x):
+        pass
---
+++ thermal/sgbm.py
@@ -0,0 +1,8 @@
+import cv2
+
+class StereoBlockMatching:
+    def __init__(self) -> None:
+        pass
+
+    def forward(self, x):
+        pass
