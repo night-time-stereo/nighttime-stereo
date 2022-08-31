79b324e6650af35d0a2780c432e10ca28e0cea27
Vanshaj Chowdhary <vanshaj.chowdhary@gmail.com>
2022-08-30 20:22:25 -0400

    Add basic metrics

--- eval.py
+++ eval.py
@@ -3,27 +3,40 @@ import numpy as np
 import cv2
 import tqdm
 import matplotlib.pyplot as plt
+import torch
 
 from model.sgbm import StereoBlockMatching
 from data.kitti_dataloader import get_kitti_files, preprocess_data
-
+from metrics.kitti_metrics import BadX, Rmse
 
 KITTI_DIR = "/mnt/aidtr/external/kitti/stereo_flow_sceneflow_2015/training/"
 kitti_dl = get_kitti_files(KITTI_DIR)
 max_disp = 128
 sgbm = StereoBlockMatching(max_disp)
 
+metrics = {'Bad-1 %': BadX(1.), 'Bad-4 %': BadX(4.), 'Rmse': Rmse()}
+
 for i in tqdm.tqdm(range(len(kitti_dl))):
-    print(kitti_dl[i])
     left, right, gt = preprocess_data(*kitti_dl[i])
     pred = sgbm.predict(left, right)
-    plt.imshow(pred, cmap='inferno', vmin=0, vmax=max_disp)
-    plt.savefig(f'debug/sgbm_{i}.png')
-    plt.close()
-    print(pred.max(), pred.min(), gt.max(), gt.min())
-    plt.imshow(gt, cmap='inferno', vmin=0, vmax=max_disp)
-    plt.savefig(f'debug/gt_{i}.png')
-    plt.close()
-    cv2.imwrite(f'debug/left_{i}.png', left)
-    cv2.imwrite(f'debug/right_{i}.png', right)
+    print(pred.shape, gt.shape)
+    for k, m in metrics.items():
+        print(f'{k}: {m(torch.from_numpy(pred), torch.from_numpy(gt))}')
+
+
+print("Final Results")
+for k, m in metrics.items():
+        print(f"{k}: {m.compute()}")
+
+
+
+
+
+    # plt.imshow(pred, cmap='inferno', vmin=0, vmax=max_disp)
+    # plt.savefig(f'debug/sgbm_{i}.png')
+    # plt.close()
+    # print(pred.max(), pred.min(), gt.max(), gt.min())
+    # plt.imshow(gt, cmap='inferno', vmin=0, vmax=max_disp)
+    # plt.savefig(f'debug/gt_{i}.png')
+    # plt.close()
 
---
+++ metrics/kitti_metrics.py
@@ -0,0 +1,34 @@
+import numpy as np
+import torch
+from torchmetrics import Metric
+
+class BadX(Metric):
+    def __init__(self, threshold):
+        super().__init__()
+        self.add_state("errors", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.threshold = threshold
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor):
+        assert preds.shape == target.shape
+        mask = target > 0
+        self.errors += torch.sum(((preds - target).abs() > self.threshold) * mask)
+        self.total += mask.sum()
+
+    def compute(self):
+        return self.errors.float() / self.total
+
+class Rmse(Metric):
+    def __init__(self):
+        super().__init__()
+        self.add_state("errors", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor):
+        assert preds.shape == target.shape
+        mask = target > 0
+        self.errors += torch.sum(((preds - target) ** 2) * mask)
+        self.total += mask.sum()
+
+    def compute(self):
+        return torch.sqrt(self.errors / self.total)
