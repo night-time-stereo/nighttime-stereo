13466c8611eac733db22e6da09124d22dcdb0366
Vanshaj Chowdhary <vanshaj.chowdhary@gmail.com>
2022-09-03 20:28:37 -0400

    Add canon dataloader

--- predict.py
+++ predict.py
@@ -1,4 +1,5 @@
 
+from this import d
 import numpy as np
 import cv2
 import tqdm
@@ -8,38 +9,32 @@ import os
 
 from model.sgbm import StereoBlockMatching
 from data.kitti_dataloader import get_kitti_files, preprocess_data
+from data.canon_dataloder import get_canon_files, preprocess_canon_data
 
-dataset = "kitti"
+
 KITTI_DIR = "/mnt/aidtr/external/kitti/stereo_flow_sceneflow_2015/training/"
-kitti_dl = get_kitti_files(KITTI_DIR)
-max_disp = 128
+CANON_DIR = "/mnt/aidtr/members/vanshajc/data/canon_images/dark/"
+
+dataset = "canon"
+
+if dataset == "kitti":
+    dl = get_kitti_files(KITTI_DIR)
+    preprocessor = preprocess_data
+elif dataset == "canon":
+    dl = get_canon_files(CANON_DIR)
+    preprocessor = preprocess_canon_data
+
+max_disp = 256
 sgbm = StereoBlockMatching(max_disp)
 
 if not os.path.exists(f'outputs/sgbm/{dataset}'):
     os.makedirs(f'outputs/sgbm/{dataset}')
 
-for i in tqdm.tqdm(range(len(kitti_dl))):
-    left, right, gt = preprocess_data(*kitti_dl[i])
+for i in tqdm.tqdm(range(len(dl))):
+    left, right = preprocessor(*dl[i])
     pred = sgbm.predict(left, right)
 
-    np.save(f'outputs/sgbm/{dataset}/{kitti_dl[i][0].split("/")[-1]}.npy', pred)
-
-    # print(pred.shape, gt.shape)
-    # for k, m in metrics.items():
-    #     print(f'{k}: {m(torch.from_numpy(pred), torch.from_numpy(gt))}')
-    # error_map = ((pred - gt) ** 2) * (gt > 0)
-    # mean_err = error_map.sum() / (gt > 0).sum()
-    # print(np.sqrt(error_map.max()), np.sqrt(error_map.min()), np.sqrt(mean_err))
-    # plt.imshow(error_map, cmap='inferno')
-    # plt.savefig(f'debug/error_{i}.png')
-    # plt.close()
-    # plt.imshow(pred, cmap='inferno', vmin=0, vmax=max_disp)
-    # plt.savefig(f'debug/sgbm_{i}.png')
-    # plt.close()
-    # print(pred.max(), pred.min(), gt.max(), gt.min())
-    # plt.imshow(gt, cmap='inferno', vmin=0, vmax=max_disp)
-    # plt.savefig(f'debug/gt_{i}.png')
-    # plt.close()
-    # break
-
-
+    np.save(f'outputs/sgbm/{dataset}/{dl[i][0].split("/")[-1]}.npy', pred)
+    plt.imshow(pred, cmap='inferno', vmin=0, vmax=max_disp)
+    plt.savefig(f'debug/dark_sgbm_{dataset}_{i}.png')
+    plt.close()
